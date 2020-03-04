54dceb36d28b146b4ad881a86e0982592b1aff3c
Ujual PS <ujual.ps@hashedin.com>
2020-03-04 14:35:53 +0530

    API conneced to new post UI

--- src/components/NewPostBox/index.tsx
+++ src/components/NewPostBox/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './styles.css';
+import axios from 'axios';
 
 interface NewPostBoxValues{
     userId: number;
@@ -7,12 +8,37 @@ interface NewPostBoxValues{
 
 
 export const NewPostBox = (props: NewPostBoxValues) => {
+    const [enteredTitle, setTitle] = React.useState("");
+    const [enteredDesc, setDesc] = React.useState("");
+    const [validateTitle, setValidateTitle] = React.useState('')
+
+
+    const handlePostClick = () =>{
+        if(!/[a-zA-z][a-zA-Z0-9 ]{5}[a-zA-Z0-9 ]*/.test(enteredTitle)){
+            setValidateTitle('Title needs minimum 6 characters')
+            return
+        }
+        else
+            setValidateTitle('')
+        axios.post('http://localhost:5000/new_post',
+        {
+            userid: props.userId,
+            title: enteredTitle,
+            desc: enteredDesc
+        }).then(function (response) {
+            setTitle('')
+            setDesc('')
+        })
+    }
+
+
     return(
         <section className="NewPostMain">
-            <input placeholder="Enter Post Title" type="text" className="PostHeading"/>
-            <input placeholder="Enter Post Description" type="text" className="PostDescription"/>
+            <input placeholder="Enter Post Title" value={enteredTitle} type="text" className="PostHeading" onChange={e => setTitle(e.target.value)}/>
+            <li className="errorMsg">{validateTitle}</li>
+            <textarea placeholder="Enter Post Description" value={enteredDesc} className="PostDescription" onChange={e => setDesc(e.target.value)}/>
             <div className='PostButtonDiv'>
-                <button className="PostButton">POST</button>
+                <button className="PostButton" onClick={() => handlePostClick()}>POST</button>
             </div>
         </section>
     )
